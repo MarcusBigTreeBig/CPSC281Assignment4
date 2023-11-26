677dd6b751e424812efaa317e3165d7cfea54b96
MarcusBigTreeBig <the.marcus.herbert@gmail.com>
2023-11-26 14:13:20 -0800

    assignment essentially done

--- src/question2/Main.java
+++ src/question2/Main.java
@@ -1,7 +1,5 @@
 package question2;
 
-import java.util.ArrayList;
-
 public class Main {
 
     /**
@@ -12,7 +10,7 @@ public class Main {
      * @param args
      */
     public static void main (String[] args) {
-        char[] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        char[] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
         int[] weights = {7, 2,  2, 3, 11, 2, 2, 6, 6, 1, 1, 4, 3, 7, 9, 2, 1, 6, 6, 8, 4, 1, 2, 1, 2, 1};
         String[] codes = huffman(weights);
         for (int i = 0; i < letters.length; i++) {
@@ -28,13 +26,13 @@ public class Main {
      */
     public static String[] huffman (int[] weights) {
         //initialize all weights as roots
-        ArrayList<IntBinaryTree> roots = new ArrayList<>();
-        ArrayList<IntBinaryTree> originalNodes = new ArrayList<>();
+        IntBinaryTree[] roots = new IntBinaryTree[weights.length];
+        IntBinaryTree[] originalNodes = new IntBinaryTree[weights.length];
         int sum = 0;
-        for (int i: weights) {
-            roots.add(new IntBinaryTree(i));
-            originalNodes.add(roots.get(roots.size()-1));
-            sum += i;
+        for (int i = 0; i < weights.length; i++) {
+            roots[i] = new IntBinaryTree(weights[i]);
+            originalNodes[i] = roots[i];
+            sum += weights[i];
         }
 
         //converting the weights into a tree until only 1 root node
@@ -45,51 +43,78 @@ public class Main {
         int current;
         IntBinaryTree tree;
 
-        while (roots.size() != 1) {
+        for (int j = 0; j < weights.length-1; j++) {
             minIndex1 = -1;//-1 if hasn't found an index yet
             minIndex2 = -1;
-            for (int i = 0; i < roots.size(); i++) {
-                current = roots.get(i).getValue();
-                if (minIndex1 > -1) {//checking if it has an index yet
-                    min1 = roots.get(minIndex1).getValue();
-                }else{
-                    min1 = sum+1;//larger value than possible
-                }
-                if (minIndex2 > -1) {
-                    min2 = roots.get(minIndex2).getValue();
-                }else{
-                    min2 = sum+1;
-                }
-                //considering all cases, these are the ones where something should be done
-                if (current <= min1 && min1 <= min2 || min1 < current && current < min1) {
-                    minIndex2 = i;
-                } else if (current <= min2 && min2 < min1 || min2 < current && current < min1) {
-                    minIndex1 = i;
+            for (int i = 0; i < roots.length; i++) {
+                if (roots[i] != null) {
+                    current = roots[i].getValue();
+                    if (minIndex1 > -1) {//checking if it has an index yet
+                        min1 = roots[minIndex1].getValue();
+                    } else {
+                        min1 = sum + 1;//larger value than possible
+                    }
+                    if (minIndex2 > -1) {
+                        min2 = roots[minIndex2].getValue();
+                    } else {
+                        min2 = sum + 1;
+                    }
+                    //considering all cases, these are the ones where something should be done
+                    if (current <= min1 && min1 <= min2 || min1 < current && current < min2) {
+                        minIndex2 = i;
+                    } else if (current <= min2 && min2 < min1 || min2 < current && current < min1) {
+                        minIndex1 = i;
+                    }
                 }
             }
-            tree = new IntBinaryTree(roots.get(minIndex1).getValue() + roots.get(minIndex2).getValue());
-            tree.setLeft(roots.get(minIndex1));
-            tree.setRight(roots.get(minIndex2));
+            tree = new IntBinaryTree(roots[minIndex1].getValue() + roots[minIndex2].getValue());
+            tree.setLeft(roots[minIndex1]);
+            tree.setRight(roots[minIndex2]);
             tree.getLeft().setParent(tree);
             tree.getRight().setParent(tree);
-            if (minIndex1 > minIndex2) {
-                roots.remove(minIndex1);
-                roots.remove(minIndex2);
-            }else{
-                roots.remove(minIndex2);
-                roots.remove(minIndex1);
-            }
-            roots.add(tree);
-
+            roots[minIndex1] = null;
+            roots[minIndex2] = null;
+            roots[minIndex2] = tree;
         }
 
         //finding the huffman value for each starting node
         String[] codes = new String[weights.length];
-        for (int i = 0; i < originalNodes.size(); i++) {
-            codes[i] = originalNodes.get(i).huffmanValue();
+        for (int i = 0; i < weights.length; i++) {
+            codes[i] = originalNodes[i].huffmanValue();
         }
 
         return codes;
     }
 
 }
+
+/*
+Output:
+
+a: 1111
+b: 011001
+c: 010101
+d: 00110
+e: 010
+f: 101001
+g: 010001
+h: 0100
+i: 0011
+j: 1000001
+k: 0111001
+l: 1100
+m: 10110
+n: 0111
+o: 000
+p: 001001
+q: 0110001
+r: 1110
+s: 1011
+t: 1101
+u: 00101
+v: 0000001
+w: 100001
+x: 1110001
+y: 110101
+z: 1111001
+ */
